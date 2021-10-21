b1f07ba82ae0aed5fa111dda11bd2ed0a0689def
obleey <miha.oblishar@gmail.com>
2021-10-21 22:10:40 +0200

    Added /signin and /register and password hashing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt-node');
 const database = {
   users: [
     {
@@ -22,7 +23,7 @@ const database = {
 const app = express();
 
 app.use(express.json());
-
+//Signin
 app.post('/signin', (req, res) => {
   if (
     req.body.email === database.users[0].email &&
@@ -31,9 +32,13 @@ app.post('/signin', (req, res) => {
     res.json('success');
   else res.status(400).json('error logging in');
 });
-
+//Register
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
+  bcrypt.hash('bacon', null, null, function (err, hash) {
+    // Store hash in your password DB.
+    console.log(hash);
+  });
   database.users.push({
     id: 125,
     name: name,
@@ -44,9 +49,38 @@ app.post('/register', (req, res) => {
   });
   res.json(database.users[database.users.length - 1]);
 });
-
+//Root
 app.get('/', (req, res) => {
   res.send(database.users);
+});
+//Profil
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json('user not found');
+  }
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json('user not found');
+  }
 });
 
 app.listen(3000, () => {
