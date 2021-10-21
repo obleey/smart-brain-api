def5e69f32df4c8346890becd43aac9b1b12b8e4
obleey <miha.oblishar@gmail.com>
2021-10-21 18:39:54 +0200

    Added /signin and /register

--- server.js
+++ server.js
@@ -21,13 +21,32 @@ const database = {
 };
 const app = express();
 
+app.use(express.json());
+
 app.post('/signin', (req, res) => {
-  req.body;
-  res.json('signin');
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  )
+    res.json('success');
+  else res.status(400).json('error logging in');
+});
+
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: 125,
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
 });
 
 app.get('/', (req, res) => {
-  res.send('this is working');
+  res.send(database.users);
 });
 
 app.listen(3000, () => {
