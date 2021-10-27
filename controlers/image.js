f5d070acd61206e29683354c5dba4631a0854cb4
Miha Oblišar <48677151+obleey@users.noreply.github.com>
2021-10-27 22:07:11 +0200

    Update image.js

--- controlers/image.js
+++ controlers/image.js
@@ -38,15 +38,6 @@ const handleImageApi = (req, res) => {
   );
 };
 
-// const handleImageApi = (req, res) => {
-//   app.models
-//     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-//     .then((data) => {
-//       res.json(data);
-//     })
-//     .catch((err) => res.status(400).json('unable to work with API'));
-// };
-
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
